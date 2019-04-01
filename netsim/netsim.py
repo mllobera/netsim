34cdcdca3353d8461ae59cee3984d0efb1b8c2a8
Marcos Llobera <mllobera@uw.edu>
2019-04-01 08:16:31 -0700

    new netsim. included average gt

--- netsim/netsim.py
+++ netsim/netsim.py
@@ -1,11 +1,33 @@
 import numpy as np
 import netsim.path_tools as pt
+from collections import OrderedDict
 from netsim.iwdt import calculate_iwdt, calculate_dt
 
-
-def netsim(pts, net_layout, cost_dict, netsim_dict):
+def calculate_network_cost(Gt, cellsize, alpha):
     '''
-    Generates a path network.
+    calculates the network cost for current ground potential Gt
+    
+    Parameters
+    ----------
+    
+    Gt: 2D numpy array
+        Ground potential
+    
+    alpha: float
+        coefficient calculated as $\frac{d_0}{ln \left 1 - NC_0 \right$ }
+    
+    '''
+   
+    d = np.full_like(Gt, 99999.0)
+    d[Gt > 1.0] = 0.0
+    d = calculate_dt(d, cellsize, option=2)
+    
+    return  1.0 - np.exp(d / alpha) *  np.mean(Gt)
+
+
+def simulation(pts, net_layout, cost_dict, netsim_dict):
+    '''
+    Generates a network of paths.
     
     Parameters
     ----------
@@ -14,26 +36,27 @@ def netsim(pts, net_layout, cost_dict, netsim_dict):
         contains the identifier, and row and column of each location
     
     net_layout: dataframe
-        dataframe specifying origin and destination of each path network
+        dataframe specifying origin and destination of each path in the network
     
     cost_dict: dictionary
-        contains parameters used for *calculate_iwdt()*
+        contains parameters used for ```calculate_iwdt()```
     
     netsim_dict: dictionary
-        contains parameters needed to execute netsim.
-        
+        contains parameters needed to execute netsim
+
+
     Returns
     -------
     
-    G_t: 2D numpy array
+    Gt: 2D numpy array
         final ground potential
     
     paths: 2D numpy array
         sum of all network paths
         
     paths_dict: dictionary
-        dictionary with the track of every path
-        
+        dictionary with the track of every path in network
+
     Notes
     -----
     
@@ -43,14 +66,14 @@ def netsim(pts, net_layout, cost_dict, netsim_dict):
     
     # unpack netsim dictionary
     i = netsim_dict['i']
-    G_max = netsim_dict['G_max']
-    ri = netsim_dict['ri']
+    Gmax = netsim_dict['Gmax']
+    T = netsim_dict['T']
     alpha = netsim_dict['alpha']
     
     # initialize netsim outputs
-    G_t = np.zeros_like(cost_dict['DEM'])
-    G_t_1 = np.zeros_like(G_t)
-    paths = np.zeros_like(G_t)
+    Gt = np.zeros_like(cost_dict['dem'])
+    Gt_1 = np.zeros_like(Gt)
+    paths = np.zeros_like(Gt)
     
     # initialize paths dictionary
     paths_dict= OrderedDict()
@@ -66,7 +89,7 @@ def netsim(pts, net_layout, cost_dict, netsim_dict):
         destination = [[ pts.loc[pts['id'] == d, 'r'].values[0], pts.loc[pts['id'] == d,'c'].values[0] ]]
                 
         # initialize ACS
-        acs = np.full_like(G_t, 999999.0)
+        acs = np.full_like(Gt, 999999.0)
         for r,c in origin:
             acs[r,c] = 0.0
         
@@ -78,23 +101,15 @@ def netsim(pts, net_layout, cost_dict, netsim_dict):
 
         # update paths
         paths += path_t
-        
-        # update paths_dict
         paths_dict.update(track)
         
         # update ground potential
-        G_t = ri * G_t_1 + path_t * i * (1 - (G_t_1 / G_max))
+        Gt = Gt_1 - (Gt_1/T) + path_t * i * (1 - (Gt_1 / Gmax))
+              
+        #update network cost
+        cost_dict['netcost'] = calculate_network_cost(Gt, cost_dict['cellsize'], alpha)
         
-        # create current network cost
-        d = np.full_like(G_t, 99999.0)
-        d[G_t > 0] = 0.0
-        d = calculate_dt(d, cellsize, option=2)
-        netcost = (G_t + 1.0) * (1.0 - np.exp(d / alpha)) # G_t *
+        # update Gt_1
+        Gt_1 = np.copy(Gt)
         
-        #update netcost
-        cost_dict['NETCOST'] = netcost
-        
-        # update G_t_1
-        G_t_1 = np.copy(G_t)
-    
-    return G_t, paths, paths_dict
+    return Gt, paths, paths_dict
